7d946792d30d840ac67fc382e0cfd9d78a7ba255
simar1998 <simarpal.kalsi@gmail.com>
2015-09-19 15:59:44 -0400

    get set methods made tedious. i do not like them.

--- RealityShowApplication/src/kalsi/ContestantInformation.java
+++ RealityShowApplication/src/kalsi/ContestantInformation.java
@@ -1,21 +1,77 @@
 package kalsi;
 
 public class ContestantInformation {
-	String firstName,lastName, city, province, postalCode, streetName, streetNumber, phoneNum, birthDate;
+	String firstName, lastName, city, province, postalCode, streetName, streetNumber, phoneNum, birthDate;
+
 	public ContestantInformation() {
 		// TODO Auto-generated constructor stub
 	}
 
-	public ContestantInformation(String firstName, String lastName, int streetNumber, String streetName, String city,
-			String province, String postalCode, int phoneNum, int birthDate) {
+	public ContestantInformation(String firstName, String lastName, int streetNumber, String streetName, 
+			String city, String province, String postalCode, int phoneNum, int birthDate) {
 		this.firstName = firstName;
 		this.lastName = lastName;
-		this.birthDate = ""+birthDate;
-		this.streetNumber = ""+streetNumber;
+		this.birthDate = "" + birthDate;
+		this.streetNumber = "" + streetNumber;
 		this.streetName = streetName;
 		this.city = city;
 		this.postalCode = postalCode;
-		this.phoneNum = ""+phoneNum;
+		this.phoneNum = "" + phoneNum;
 	}
 
-}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public void setLastName(String lastName){
+		this.lastName = lastName;
+	}
+	public void setStreetNumber(String streetNumber){
+		this.streetNumber = streetNumber;
+	}
+	public void setStreetName(String streetName){
+		this.streetName = streetName;
+	}
+	public void setCity(String city){
+		this.city = city;
+	}
+	public void setProvince(String province){
+		this.province = province;
+	}
+	public void setPostalCode(String postalCode){
+		this.postalCode = postalCode;
+	}
+	public void setPhoneNum(String phoneNum){
+		this.phoneNum = phoneNum;
+	}
+	public void setBirthDate(String birthDate){
+		this.birthDate = birthDate;
+	}
+	public String getFirstName(){
+		return this.firstName;
+	}
+	public String getLastName(){
+		return this.lastName;
+	}
+	public 	String getStreetNumber(){
+		return this.streetName;
+	}
+	public String getStreetName(){
+		return this.streetName;
+	}
+	public String getCity(){
+		return this.city;
+	}
+	public String getProvince(){
+		return this.province;
+	}
+	public String getPostalCode(){
+		return this.postalCode;
+	}
+	public String getPhoneNum(){
+		return this.phoneNum;
+	}
+	public String getBirthDate(){
+	return this.birthDate;
+	}
+	}
+	
