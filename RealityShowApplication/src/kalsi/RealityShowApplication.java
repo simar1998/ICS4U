40428ab3687745ed5146ecf3a55c40f0b5a54f63
Simar Pal Kalsi <simarpal.kalsi@gmail.com>
2015-10-02 13:09:31 -0400

    Added reflections for invoking all the set methods in the ContestantInformation class.

--- RealityShowApplication/src/kalsi/RealityShowApplication.java
+++ RealityShowApplication/src/kalsi/RealityShowApplication.java
@@ -3,6 +3,7 @@
  */
 package kalsi;
 
+import java.lang.reflect.InvocationTargetException;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -21,17 +22,45 @@ public class RealityShowApplication {
 
 	/**
 	 * @param args
+	 * @throws SecurityException 
+	 * @throws NoSuchMethodException 
+	 * @throws InvocationTargetException 
+	 * @throws IllegalArgumentException 
+	 * @throws IllegalAccessException 
+	 * @throws ClassNotFoundException 
+	 * @throws InstantiationException 
 	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
+	NoSuchMethodException, SecurityException, InvalidInputExeption, ClassNotFoundException, InstantiationException {
 		// TODO Auto-generated method stub
 		ContestantInformation contestant1 = new ContestantInformation();
 		ContestantInformation contestant2 = new ContestantInformation();
 		Scanner scanner = new Scanner(System.in);
 		String[] questions = { "What is your first name", "What is your last name",
-				"What is your Street Name" + "What is your Sreet Number", "What city do you live in",
+				"What is your Street Name" , "What is your Sreet Number", "What city do you live in",
 				"What province are you in", "What is your postal code", "What is your phone no.",
 				"What is your birth date yyyy/mm/dd" };
+		String[] methods = {"setFirstName","setLastName","setStreeName",
+				"setStreetNumber","setCity","setProvince","setPostalCode","setPhoneNum",
+				"setBirthDate"};
+	
+		Class<?> classContestantInfo = Class.forName("kalsi.ContestantInformation");
+		Object objectCI = classContestantInfo.newInstance();
+		Class<String> parameters = String.class;
+		boolean flag = true;
+		for(int i = 0; i <methods.length;i++){
+			do{
+				flag = false;
+				System.out.println(questions[i]);
+				String scannerInput = scanner.nextLine();
+				classContestantInfo.getDeclaredMethod(methods[i],parameters).invoke(contestant1, scannerInput);
+				}
+			while(flag);
+		}
+		
+		
+		}
 	
 	}
 
-}
+
