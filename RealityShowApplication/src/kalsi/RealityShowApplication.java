f9bdd670e1b11c3a164863ac7f0cdba13aebb5ec
Simar Pal Kalsi <simarpal.kalsi@gmail.com>
2015-10-02 13:25:20 -0400

    added try catch

--- RealityShowApplication/src/kalsi/RealityShowApplication.java
+++ RealityShowApplication/src/kalsi/RealityShowApplication.java
@@ -22,45 +22,44 @@ public class RealityShowApplication {
 
 	/**
 	 * @param args
-	 * @throws SecurityException 
-	 * @throws NoSuchMethodException 
-	 * @throws InvocationTargetException 
-	 * @throws IllegalArgumentException 
-	 * @throws IllegalAccessException 
-	 * @throws ClassNotFoundException 
-	 * @throws InstantiationException 
+	 * @throws SecurityException
+	 * @throws NoSuchMethodException
+	 * @throws InvocationTargetException
+	 * @throws IllegalArgumentException
+	 * @throws IllegalAccessException
+	 * @throws ClassNotFoundException
+	 * @throws InstantiationException
 	 */
-	public static void main(String[] args) throws IllegalAccessException, IllegalArgumentException, InvocationTargetException,
-	NoSuchMethodException, SecurityException, InvalidInputExeption, ClassNotFoundException, InstantiationException {
+	public static void main(String[] args)
+			throws IllegalAccessException, IllegalArgumentException, InvocationTargetException, NoSuchMethodException,
+			SecurityException, InvalidInputExeption, ClassNotFoundException, InstantiationException {
 		// TODO Auto-generated method stub
 		ContestantInformation contestant1 = new ContestantInformation();
 		ContestantInformation contestant2 = new ContestantInformation();
 		Scanner scanner = new Scanner(System.in);
-		String[] questions = { "What is your first name", "What is your last name",
-				"What is your Street Name" , "What is your Sreet Number", "What city do you live in",
-				"What province are you in", "What is your postal code", "What is your phone no.",
-				"What is your birth date yyyy/mm/dd" };
-		String[] methods = {"setFirstName","setLastName","setStreetName",
-				"setStreetNumber","setCity","setProvince","setPostalCode","setPhoneNum",
-				"setBirthDate"};
+		String[] questions = { "What is your first name", "What is your last name", "What is your Street Name",
+				"What is your Sreet Number", "What city do you live in", "What province are you in",
+				"What is your postal code", "What is your phone no.", "What is your birth date yyyy/mm/dd" };
+		String[] methods = { "setFirstName", "setLastName", "setStreetName", "setStreetNumber", "setCity",
+				"setProvince", "setPostalCode", "setPhoneNum", "setBirthDate" };
 
 		Class<?> classContestantInfo = Class.forName("kalsi.ContestantInformation");
 		Object objectCI = classContestantInfo.newInstance();
 		Class<String> parameters = String.class;
 		boolean flag = true;
-		for(int i = 0; i <methods.length;i++){
-			do{
-				flag = false;
-				System.out.println(questions[i]);
-				String scannerInput = scanner.nextLine();
-				classContestantInfo.getDeclaredMethod(methods[i],parameters).invoke(contestant1, scannerInput);
+		for (int i = 0; i < methods.length; i++) {
+			do {
+				try {
+					flag = false;
+					System.out.println(questions[i]);
+					String scannerInput = scanner.nextLine();
+					classContestantInfo.getDeclaredMethod(methods[i], parameters).invoke(contestant1, scannerInput);
+				} catch (InvalidInputExeption e) {
+					System.out.println(e.getMessage());
 				}
-			while(flag);
+			} while (flag);
 		}
-		
-		
-		}
-	
+
 	}
 
-
+}
