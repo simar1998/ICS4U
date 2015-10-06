2d67f8355a8b5cd401a91e61984556ba7afbd7c2
Simar Pal Kalsi <simarpal.kalsi@gmail.com>
2015-10-06 13:41:45 -0400

    added stuff

--- RealityShowApplication/src/kalsi/RealityShowApplication.java
+++ RealityShowApplication/src/kalsi/RealityShowApplication.java
@@ -35,29 +35,44 @@ public class RealityShowApplication {
 			SecurityException, ClassNotFoundException, InstantiationException {
 		// TODO Auto-generated method stub
 		ContestantInformation contestant1 = new ContestantInformation();
-		ContestantInformation contestant2 = new ContestantInformation();
-		ArrayList <ContestantInformation> contestants = new ArrayList();
+		ArrayList<ContestantInformation> contestants = new ArrayList();
 		int contestantNum = 0;
 		Scanner scannerInput = new Scanner(System.in);
-		String inp = scannerInput.nextLine();
-		for(int i = 0; i <= contestantNum; i++){
-			switch(inp){
-			case "add" :
-				storeValues(contestant1);
+		for (int i = 0; i <= contestantNum; i++) {
+			help();
+			pl(i+"");
+			String inp = scannerInput.nextLine();
+			contestantNum = contestants.size() -1;
+		
+			if(inp.equals("add new")){
+				contestants.add(contestant1);
+				storeValues(contestants.get(i));
 			}
-		}
-		storeValues(contestant1);
-		 		
-		}
-	public static void storeValues(ContestantInformation ci) throws IllegalAccessException,
-	IllegalArgumentException, NoSuchMethodException, SecurityException, ClassNotFoundException{
+
+			if(inp.equals("get last")){
+				printContestant(contestants.get(i));
+			}
+
+			if(inp.equals("get contestant")){
+				printContestant(contestants);
+			}
+
+			if(inp.equals("help")){
+				help();
+			}
+			}
+		
+	}
+
+	public static void storeValues(ContestantInformation ci) throws IllegalAccessException, IllegalArgumentException,
+			NoSuchMethodException, SecurityException, ClassNotFoundException {
 		Scanner scanner = new Scanner(System.in);
 		String[] questions = { "What is your first name", "What is your last name", "What is your Street Name",
 				"What is your Sreet Number", "What city do you live in", "What province are you in",
 				"What is your postal code", "What is your phone no.", "What is your birth date yyyy/mm/dd" };
 		String[] methods = { "setFirstName", "setLastName", "setStreetName", "setStreetNumber", "setCity",
 				"setProvince", "setPostalCode", "setPhoneNum", "setBirthDate" };
-		
+
 		Class<?> classContestantInfo = Class.forName("kalsi.ContestantInformation");
 		Class<String> stringParameter = String.class;
 		Class<Integer> integerParameter = Integer.class;
@@ -65,25 +80,48 @@ public class RealityShowApplication {
 		for (int i = 0; i < methods.length; i++) {
 			flag = true;
 			do {
-				try{
-				System.out.println(questions[i]);
-				String scannerInput = scanner.nextLine();
-				classContestantInfo.getDeclaredMethod(methods[i], stringParameter).invoke(ci, scannerInput);
-				flag = false;
-				if(scannerInput.equals("exit this")){
-					System.out.println("Thank you for using reality show application form");
-					return;
+				try {
+					System.out.println(questions[i]);
+					String scannerInput = scanner.nextLine();
+					classContestantInfo.getDeclaredMethod(methods[i], stringParameter).invoke(ci, scannerInput);
+					flag = false;
+					if (scannerInput.equals("exit this")) {
+						pl("Thank you for using reality show application form");
+						return;
+					}
+				} catch (InvocationTargetException e) {
+					if (e.getCause() instanceof InvalidInputExeption) {
+						System.out.println(e.getCause().getMessage());
+					}
 				}
-				}	catch(InvocationTargetException e){
-					 if(e.getCause() instanceof InvalidInputExeption){
-						 System.out.println(e.getCause().getMessage());
-					 }
-				}
-			}
-			 while (flag);
-			}
+			} while (flag);
+		}
 	}
-	
+
+	public static void printContestant(ContestantInformation ci) {
+		LabelClass label = new LabelClass(ci);
+		System.out.print(label.toString());
+	}
+
+	public static void printContestant(ArrayList<ContestantInformation> c) {
+		pl("What contestant would you like to get the info of");
+		Scanner scanner = new Scanner(System.in);
+		int i = Integer.parseInt(scanner.nextLine());
+		LabelClass label = new LabelClass(c.get(i));
+		System.out.print(label.toString());
+	}
+
+	public static void help() {
+		pl("REALITY SHOW APPLICATION HELP");
+		pl("Type (add new) to add new contestant");
+		pl("Type (get last) to get the last stored contestant's information");
+		pl("type (get contestant) hit enter then type the number id of the contestant to retrive the contestant info");
+		pl("type (exit this) to exit");
 
 	}
 
+	public static void pl(String s) {
+		System.out.println(s);
+	}
+
+}
