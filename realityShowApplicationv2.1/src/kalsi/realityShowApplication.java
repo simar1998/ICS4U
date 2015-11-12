87b0bb045b87d411b4e76b805329577923cca93c
simar1998 <simarpal.kalsi@gmail.com>
2015-11-11 23:24:37 -0500

    gui adds to the contestant information array now working on updating list

--- realityShowApplicationv2.1/src/kalsi/realityShowApplication.java
+++ realityShowApplicationv2.1/src/kalsi/realityShowApplication.java
@@ -1,11 +1,12 @@
 package kalsi;
+
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
-
 
 import kalsi.ContestantInformation;
 import kalsi.InvalidInputExeption;
@@ -23,12 +24,30 @@ import javax.swing.JOptionPane;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import java.awt.Panel;
 
 public class realityShowApplication extends JFrame {
 
 	private JPanel contentPane;
 	private TextField cityTextField;
+	private TextField firstNameTextField;
+	private TextField lastNameTextField;
+	private TextField streetNameTextField;
+	private TextField streetNumberTextField;
+	private TextField provinceTextField;
+	private TextField postalCodeTextField;
+	private TextField poneNumberTextField;
+	private TextField birthDateTextField;
 
+	String firstName = "";
+	String lastName = "";
+	String streetName = "";
+	String streetNumber = "";
+	String city = "";
+	String province = "";
+	String postalCode = "";
+	String phoneNumber = "";
+	String birthDate = "";
 	/**
 	 * Launch the application.
 	 */
@@ -50,13 +69,13 @@ public class realityShowApplication extends JFrame {
 	 */
 	public realityShowApplication() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1050, 436);
+		setBounds(100, 100, 1050, 540);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 
-		TextField firstNameTextField = new TextField();
+	    firstNameTextField = new TextField();
 		firstNameTextField.setBounds(10, 64, 181, 22);
 		contentPane.add(firstNameTextField);
 
@@ -64,7 +83,7 @@ public class realityShowApplication extends JFrame {
 		label.setBounds(197, 64, 94, 22);
 		contentPane.add(label);
 
-		TextField lastNameTextField = new TextField();
+	    lastNameTextField = new TextField();
 		lastNameTextField.setBounds(10, 92, 181, 22);
 		contentPane.add(lastNameTextField);
 
@@ -72,7 +91,7 @@ public class realityShowApplication extends JFrame {
 		label_1.setBounds(197, 92, 94, 22);
 		contentPane.add(label_1);
 
-		TextField streetNameTextField = new TextField();
+		streetNameTextField = new TextField();
 		streetNameTextField.setBounds(10, 120, 181, 22);
 		contentPane.add(streetNameTextField);
 
@@ -80,7 +99,7 @@ public class realityShowApplication extends JFrame {
 		label_2.setBounds(197, 120, 94, 22);
 		contentPane.add(label_2);
 
-		TextField streetNumberTextField = new TextField();
+		streetNumberTextField = new TextField();
 		streetNumberTextField.setBounds(10, 148, 181, 22);
 		contentPane.add(streetNumberTextField);
 
@@ -96,7 +115,7 @@ public class realityShowApplication extends JFrame {
 		label_4.setBounds(197, 176, 62, 22);
 		contentPane.add(label_4);
 
-		TextField provinceTextField = new TextField();
+		provinceTextField = new TextField();
 		provinceTextField.setBounds(10, 204, 181, 22);
 		contentPane.add(provinceTextField);
 
@@ -104,7 +123,7 @@ public class realityShowApplication extends JFrame {
 		label_5.setBounds(197, 204, 78, 22);
 		contentPane.add(label_5);
 
-		TextField postalCodeTextField = new TextField();
+		postalCodeTextField = new TextField();
 		postalCodeTextField.setBounds(10, 232, 181, 22);
 		contentPane.add(postalCodeTextField);
 
@@ -112,7 +131,7 @@ public class realityShowApplication extends JFrame {
 		label_6.setBounds(197, 232, 94, 22);
 		contentPane.add(label_6);
 
-		TextField poneNumberTextField = new TextField();
+		poneNumberTextField = new TextField();
 		poneNumberTextField.setBounds(10, 260, 181, 22);
 		contentPane.add(poneNumberTextField);
 
@@ -120,7 +139,7 @@ public class realityShowApplication extends JFrame {
 		label_7.setBounds(197, 260, 111, 22);
 		contentPane.add(label_7);
 
-		TextField birthDateTextField = new TextField();
+		birthDateTextField = new TextField();
 		birthDateTextField.setBounds(10, 288, 181, 22);
 		contentPane.add(birthDateTextField);
 
@@ -143,89 +162,112 @@ public class realityShowApplication extends JFrame {
 		panel.setLayout(null);
 
 		JList contestantsList = new JList();
-		contestantsList.setBounds(10, 66, 232, 289);
+		contestantsList.setLayoutOrientation(JList.HORIZONTAL_WRAP);
+		contestantsList.setBounds(10, 11, 232, 344);
+		JScrollPane listScroller = new JScrollPane(contestantsList);
 		panel.add(contestantsList);
-
-		TextField searchFirstNameTextField = new TextField();
-		searchFirstNameTextField.setBounds(10, 10, 112, 22);
-		panel.add(searchFirstNameTextField);
-
+		
+		Label InformationOutputLabel = new Label("");
+		InformationOutputLabel.setBounds(248, 10, 425, 346);
+		panel.add(InformationOutputLabel);
+		
+		Panel panel_1 = new Panel();
+		panel_1.setBounds(10, 385, 304, 106);
+		contentPane.add(panel_1);
+		panel_1.setLayout(null);
+		
+		TextField standardSaveLocation = new TextField();
+		standardSaveLocation.setBounds(10, 10, 172, 22);
+		panel_1.add(standardSaveLocation);
+		
+		Label label_12 = new Label("Save Location");
+		label_12.setBounds(188, 10, 92, 22);
+		panel_1.add(label_12);
+		
+		Button saveButton = new Button("Save");
+		saveButton.setBounds(10, 38, 80, 22);
+		panel_1.add(saveButton);
+		
+		Button loadButton = new Button("Load");
+		loadButton.setBounds(96, 38, 86, 22);
+		panel_1.add(loadButton);
+		
+		Button sortButton = new Button("Sort Button");
+		sortButton.setBounds(10, 66, 172, 30);
+		panel_1.add(sortButton);
+		
+		Panel panel_2 = new Panel();
+		panel_2.setBounds(322, 383, 228, 108);
+		contentPane.add(panel_2);
+		panel_2.setLayout(null);
+		
+		TextField textField = new TextField();
+		textField.setBounds(10, 10, 112, 22);
+		panel_2.add(textField);
+		
 		Label label_10 = new Label("Search first name");
 		label_10.setBounds(128, 10, 114, 22);
-		panel.add(label_10);
-
-		TextField searchLastNameTextField = new TextField();
-		searchLastNameTextField.setBounds(10, 38, 112, 22);
-		panel.add(searchLastNameTextField);
-
+		panel_2.add(label_10);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(10, 38, 112, 22);
+		panel_2.add(textField_1);
+		
 		Label label_11 = new Label("Search last name");
 		label_11.setBounds(128, 38, 104, 22);
-		panel.add(label_11);
+		panel_2.add(label_11);
 		
-		Button submitSearchButton = new Button("Search");
-		submitSearchButton.setBounds(248, 10, 70, 22);
-		panel.add(submitSearchButton);
+		Button button = new Button("Search");
+		button.setBounds(10, 66, 209, 32);
+		panel_2.add(button);
+		
+		Panel panel_3 = new Panel();
+		panel_3.setBounds(556, 401, 449, 90);
+		contentPane.add(panel_3);
+		
+		Label label_13 = new Label("Advanced Search");
+		label_13.setBounds(727, 383, 102, 22);
+		contentPane.add(label_13);
 		this.setTitle("Reality Show Application");
-		
-		Scanner scanner = new Scanner(System.in);
-		TextField[] textField = {firstNameTextField,lastNameTextField,streetNameTextField,streetNumberTextField,cityTextField,
-				provinceTextField,postalCodeTextField,poneNumberTextField,birthDateTextField};
-		String[] methods = { "setFirstName", "setLastName", "setStreetName", "setStreetNumber", "setCity",
-				"setProvince", "setPostalCode", "setPhoneNum", "setBirthDate" };
+		ContestantInformation contestant1 = new ContestantInformation();
 		ArrayList<ContestantInformation> contestants = new ArrayList();
-		ContestantInformation contestantInformation = new ContestantInformation();
+		Scanner scanner = new Scanner(System.in);
+		String[] answers = new String[9];
+		TextField[] inputField = { firstNameTextField, lastNameTextField, streetNameTextField, streetNumberTextField,
+				cityTextField, provinceTextField, postalCodeTextField, poneNumberTextField, birthDateTextField };
+		
+		for(int i = 0; i < inputField.length;i++){
+			inputField[i].setText("");
+		}
 		submitButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				contestants.add(contestantInformation);
-				Class<?> classContestantInfo = null;
-				try {
-					classContestantInfo = Class.forName("kalsi.ContestantInformation");
-				} catch (ClassNotFoundException e1) {
-					// TODO Auto-generated catch block
-					e1.printStackTrace();
-				}
-				Class<String> stringParameter = String.class;
-				Class<Integer> integerParameter = Integer.class;
-				boolean flag = true;
-				for (int i = 0; i < methods.length; i++) {
-					flag = true;
-					do {
-						try {
-							String inputTextField = scanner.nextLine();
-							inputTextField = textField[i].getName().toString();
-							Object invoke = classContestantInfo.getDeclaredMethod(methods[i], stringParameter).invoke(contestants.get(i), inputTextField);
-							flag = false;
-						} catch (InvocationTargetException e) {
-							if (e.getCause() instanceof InvalidInputExeption) {
-								JOptionPane.showMessageDialog(getContentPane(), e.getCause().getMessage());
-							}
-						} catch (IllegalAccessException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (IllegalArgumentException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (NoSuchMethodException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						} catch (SecurityException e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					} while (flag);
-				}
+				answers[0] = firstNameTextField.getText().toString();
+				answers[1] = lastNameTextField.getText().toString();
+				answers[2] = streetNameTextField.getText().toString();
+				answers[3] = streetNumberTextField.getText().toString();
+				answers[4] = cityTextField.getText().toString();
+				answers[5] = provinceTextField.getText().toString();
+				answers[6] = postalCodeTextField.getText().toString();
+				answers[7] = poneNumberTextField.getText().toString();
+				answers[8] = birthDateTextField.getText().toString();
+				
+			   try {
+				   contestants.add(contestant1);
+				storeValues(contestants.get(contestants.size()-1) , answers);
+			} catch (IllegalAccessException | IllegalArgumentException | NoSuchMethodException | SecurityException
+					| ClassNotFoundException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
 			}
 		});
 
 	}
 
-	public static void storeValues(ContestantInformation ci) throws IllegalAccessException, IllegalArgumentException,
-			NoSuchMethodException, SecurityException, ClassNotFoundException {
+	public static void storeValues(ContestantInformation ci, String[] answersArray) throws IllegalAccessException,
+			IllegalArgumentException, NoSuchMethodException, SecurityException, ClassNotFoundException {
 		Scanner scanner = new Scanner(System.in);
-		
-		String[] questions = { "What is your first name", "What is your last name", "What is your Street Name",
-				"What is your Sreet Number", "What city do you live in", "What province are you in",
-				"What is your postal code", "What is your phone no.", "What is your birth date yyyy/mm/dd" };
+
 		String[] methods = { "setFirstName", "setLastName", "setStreetName", "setStreetNumber", "setCity",
 				"setProvince", "setPostalCode", "setPhoneNum", "setBirthDate" };
 
@@ -237,23 +279,25 @@ public class realityShowApplication extends JFrame {
 			flag = true;
 			do {
 				try {
-					System.out.println(questions[i]);
-					String scannerInput = scanner.nextLine();
-					classContestantInfo.getDeclaredMethod(methods[i], stringParameter).invoke(ci, scannerInput);
+					classContestantInfo.getDeclaredMethod(methods[i], stringParameter).invoke(ci, answersArray[i]);
 					flag = false;
-					if (scannerInput.equals("exit this")) {
-						pl("Thank you for using reality show application form");
-						return;
-					}
 				} catch (InvocationTargetException e) {
 					if (e.getCause() instanceof InvalidInputExeption) {
-						System.out.println(e.getCause().getMessage());
+						JOptionPane.showMessageDialog(null, e.getCause().getMessage());
 					}
 				}
 			} while (flag);
 		}
 	}
+
 	public static void pl(String s) {
 		System.out.println(s);
+	}
+
+	public static void updateList(JList list, ArrayList<ContestantInformation> contestants){
+		String[] contestantString = new String[contestants.size()];
+		for(int i = 0; i < contestants.size();i++){
+			
+		}
 	}
 }
